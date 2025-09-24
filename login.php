5db7c0b42fc0cd47c5b1d7a188e3a04231dda37a
Jon White <57672422+jtbnz@users.noreply.github.com>
2025-09-24 20:16:26 +1200

    Add autocomplete attributes to login form inputs for improved user experience

--- login.php
+++ login.php
@@ -114,11 +114,11 @@ if (Auth::isLoggedIn()) {
         <form method="POST" action="">
             <div class="form-group">
                 <label for="username">Username</label>
-                <input type="text" id="username" name="username" required autofocus>
+                <input type="text" id="username" name="username" autocomplete="username" required autofocus>
             </div>
             <div class="form-group">
                 <label for="password">Password</label>
-                <input type="password" id="password" name="password" required>
+                <input type="password" id="password" name="password" autocomplete="current-password" required>
             </div>
             <button type="submit" class="btn-login">Sign In</button>
         </form>
